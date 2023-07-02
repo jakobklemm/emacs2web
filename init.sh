ff350bfdd9cec9a5f06a38285d02a7004c6ae3dd
Lukas <31806817+siliwan@users.noreply.github.com>
2023-07-02 14:58:51 +0200

    Update init.sh
    Removed shitty code

--- init.sh
+++ init.sh
@@ -4,10 +4,6 @@ export BROADWAYDPORT=8085
 export BRADWAYDADDRESS="http://localhost/"
 export DISPLAYNUMBER=":5"
 
-# broadwayd --port $BROADWAYDPORT --address $BROADWAYDADDRESS $DISPLAYNUMBER \
-    #     && GDK_BACKEND=broadway BROADWAY_DISPLAY=$DISPLAYNUMBER emacs
-
-echo "TEST TEST"
 echo "$DISPLAY"
 
 broadwayd --port 8085 :7 &
