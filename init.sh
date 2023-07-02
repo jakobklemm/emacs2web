ade15d1fc3b9c213fc7602e3c6e36b0d8ec8e0d6
Jakob Klemm <github@jeykey.net>
2023-07-02 17:17:34 +0200

    disl.y

--- init.sh
+++ init.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/sh
 
-export BROADWAYDPORT=8085
-export BRADWAYDADDRESS="http://localhost/"
-export DISPLAYNUMBER=":5"
-
 # broadwayd --port $BROADWAYDPORT --address $BROADWAYDADDRESS $DISPLAYNUMBER \
     #     && GDK_BACKEND=broadway BROADWAY_DISPLAY=$DISPLAYNUMBER emacs
 
-echo "TEST TEST"
 echo "$DISPLAY"
 
-broadwayd --port 8085 :7 &
-GDK_BACKEND=broadway BROADWAY_DISPLAY=:7 emacs
+gtk4-broadwayd :5
+
+# broadwayd --port 8085 :7 &
+# GDK_BACKEND=broadway BROADWAY_DISPLAY=:7 emacs
+
+GDK_BACKEND=broadway BROADWAY_DISPLAY=:5 emacs
